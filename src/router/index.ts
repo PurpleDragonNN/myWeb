07f57529d23df9d8638dcfd97b80636bbff12be7
yangzilong <yangzilong@richinfo.com>
2022-05-10 23:09:42 +0800

    feat:打包radius页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: '/index'
+            redirect: '/radius'
         },
         {
             path: '/radius',
