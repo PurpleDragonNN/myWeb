0d04c89bedb1e1db7474fee65c91e09c505a30c6
yangzilong <yangzilong@richinfo.com>
2022-05-10 23:39:28 +0800

    feat:打包radius页面

--- src/views/radius/index.vue
+++ src/views/radius/index.vue
@@ -62,7 +62,7 @@ onMounted(() => {
     reader.addEventListener('load',() => {
       //预览图片链接
       console.log(reader.result);
-      beforeUrl = reader.result
+      beforeUrl.value = reader.result
       setTimeout(() => {
         let afterTransform:any = circleRect_image({
           img: beforeImgEl!.value!,
