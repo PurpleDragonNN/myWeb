4982ffdfd0566e5f0455edf1df0d97f3eb96cd95
杨紫龙 <yangzilong@richinfo.cn>
2022-10-11 16:21:51 +0800

    feat:部署

--- docs/assets/index.5b8f405e.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./index.11ad2eb9.js";const c=t=>new a.Object(t),r=t=>new a.Query(t),o=(t,e)=>a.Object.createWithoutData(t,e);export{c as a,o as b,r as c};
---
+++ docs/assets/index.8df24068.js
@@ -0,0 +1,1 @@
+import{H as a}from"./index.3c913b2c.js";const c=t=>new a.Object(t),r=t=>new a.Query(t),o=(t,e)=>a.Object.createWithoutData(t,e);export{c as a,o as b,r as c};
